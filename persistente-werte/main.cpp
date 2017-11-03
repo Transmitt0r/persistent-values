ee32b5f56ff3d9faebe9e0c94fce3ffe4d8a4038
Lukas Grotz <lukasgrotz@gmail.com>
2017-11-03 13:29:51 +0100

    Initial commit

---
+++ persistente-werte/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  persistente-werte
+//
+//  Created by Lukas Grotz on 03.11.17.
+//  Copyright © 2017 Lukas Grotz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
